c93d8fc12478755b322f399a4ddb40a7b8ef9957
Rushaliny <shalinithiruchelvam2001@gmail.com>
2025-06-19 21:55:36 +0530

    commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!-- resources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>@yield('title', 'Billing System')</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+        name='viewport' />
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="{{ asset('assets/css/ready.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}">
+</head>
+
+<body>
+    <div class="wrapper">
+        @include('layouts.header')
+        @include('layouts.sidebar')
+
+        <div class="main-panel">
+            <div class="content">
+                @yield('content')
+            </div>
+            @include('layouts.footer')
+        </div>
+    </div>
+
+    <!-- Scripts (if any) -->
+    <script src="{{ asset('assets/js/core/jquery.3.2.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/jquery.3.2.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/chartist/chartist.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/chartist/plugin/chartist-plugin-tooltip.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/jquery-mapael/jquery.mapael.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/jquery-mapael/maps/world_countries.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/chart-circle/circles.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
+    <script src="{{ asset('assets/js/ready.min.js') }}"></script>
+    <script src="{{ asset('assets/js/demo.js') }}"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/about', function () {
-    return view('about');
+Route::get('/table', function () {
+    return view('table');
 });
+Route::get('/notifications', function () {
+    return view('notifications');
+});
+Route::get('/icons', function () {
+    return view('icons');
+});
+Route::get('/typography', function () {
+    return view('typography');
+});
+Route::get('/forms', function () {
+    return view('forms');
+});
+
+
+
